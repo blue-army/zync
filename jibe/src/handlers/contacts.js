e6716f1a74f4b4355f122be283ceff3e03bfb70c
Lucas Natraj <lnatraj@slb.com>
2017-05-31 15:45:29 -0700

    access db key from env

--- jibe/src/handlers/contacts.js
+++ jibe/src/handlers/contacts.js
@@ -9,14 +9,17 @@ var repository = require('../lib/contactRepository');
 module.exports = {
     get: function contacts_get(req, res) {
 
+        var db_key = process.env.db_key;
+
         var docDbClient = new DocDBClient(
             'https://zync.documents.azure.com:443/', {
-                masterKey: 'iR5wKpk619PBuIUqu2b5Dec79sXGsHK1P9jdzvmO1m1YBYeS8spFcP9FscoySw7SvOzka1thg14WNMtgCuRitQ=='
+                masterKey: db_key
             }
         );
 
         var uri = UriFactory.createDocumentUri('jibe', 'projects', '7aff6973-64a0-9cce-d767-a4c01622d7ed');
         docDbClient.readDocument(uri, function (err, doc) {
+            doc['db_key'] = db_key;
             res.json(doc);
         });
     }
