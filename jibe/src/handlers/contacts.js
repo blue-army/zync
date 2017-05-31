8edee6e850e282a0138547b33c2ee74254677210
Lucas Natraj <lnatraj@slb.com>
2017-05-31 15:38:30 -0700

    update

--- jibe/src/handlers/contacts.js
+++ jibe/src/handlers/contacts.js
@@ -1,9 +1,23 @@
- 'use strict';
+'use strict';
 
- var repository = require('../lib/contactRepository');
+var docdb = require('documentdb');
+var DocDBClient = docdb.DocumentClient;
+var UriFactory = docdb.UriFactory;
 
- module.exports = {
-     get: function contacts_get(req, res) {
-         res.json(repository.all());
-     }
- };
+var repository = require('../lib/contactRepository');
+
+module.exports = {
+    get: function contacts_get(req, res) {
+
+        var docDbClient = new DocDBClient(
+            'https://zync.documents.azure.com:443/', {
+                masterKey: 'iR5wKpk619PBuIUqu2b5Dec79sXGsHK1P9jdzvmO1m1YBYeS8spFcP9FscoySw7SvOzka1thg14WNMtgCuRitQ=='
+            }
+        );
+
+        var uri = UriFactory.createDocumentUri('jibe', 'projects', '7aff6973-64a0-9cce-d767-a4c01622d7ed');
+        docDbClient.readDocument(uri, function (err, doc) {
+            res.json(doc);
+        });
+    }
+};
