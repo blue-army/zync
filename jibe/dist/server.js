ff87d4c1f495de04eae2658a0f20c00f33b5eee2
Lucas Natraj <lnatraj@slb.com>
2017-06-06 14:08:47 -0700

    hope

---
+++ jibe/dist/server.js
@@ -0,0 +1,32 @@
+'use strict';
+var fs = require('fs');
+console.log(fs.existsSync);
+var Http = require('http');
+var Express = require('express');
+var BodyParser = require('body-parser');
+var Swaggerize = require('swaggerize-express');
+var SwaggerUi = require('swaggerize-ui');
+var Path = require('path');
+var port = process.env.PORT || 8000;
+// set process folder to current directory
+process.chdir(__dirname);
+var App = Express();
+var Server = Http.createServer(App);
+App.use(BodyParser.json());
+App.use(BodyParser.urlencoded({
+    extended: true
+}));
+App.use(Swaggerize({
+    api: Path.resolve('./config/swagger.json'),
+    handlers: Path.resolve('./handlers'),
+    docspath: '/swagger'
+}));
+App.use('/docs', SwaggerUi({
+    docs: '/swagger'
+}));
+Server.listen(port, function () {
+    /* eslint-disable no-console */
+    console.log('Server running on %d', port);
+    /* eslint-disable no-console */
+});
+//# sourceMappingURL=server.js.map
