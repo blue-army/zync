f0cb274865b4df4070c9a4695fb8d171b473b705
Lucas Natraj <lnatraj@slb.com>
2017-06-27 12:26:04 -0700

    add api key auth

---
+++ jibe/src/handlers/auth.ts
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = function info_get(_req: any, res: any, next) {
+    next();
+};
--- jibe/src/handlers/info.ts
+++ jibe/src/handlers/info.ts
@@ -1,15 +1,10 @@
 'use strict';
 
-import * as auth_utils from "../utils/auth-utils";
-
 module.exports = {
     get: async function info_get(_req: any, res: any) {
 
-        let token = await auth_utils.getToken();
-
         res.json({
             status: 'ok',
-            token: token,
         });
     }
 };
