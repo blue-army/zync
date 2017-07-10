e8a975cee712825efb75eed395673720f9a765a4
Lucas Natraj <lnatraj@slb.com>
2017-07-10 13:11:43 -0700

    round 4

--- jibe/scripts/build.sh
+++ jibe/scripts/build.sh
@@ -4,16 +4,18 @@ tsc
 
 cp -r ./src/config ./dist/
 
-# npm install -g polymer-cli
+# npm install bower
+# npm install polymer-cli
 
 # build web
 cd src/web
-npm install bower
-npm install polymer-cli
 
-bower install
-polymer build --name .
+# node ../../node_modules/polymer-cli/bin/polymer.js build --name .
+../../node_modules/.bin/bower install
+../../node_modules/.bin/polymer build --name .
 
 mkdir -p ../../dist/web
 cp -rf ./build/* ../../dist/web
 # node ./node_modules/polymer-cli/bin/polymer.js build --name .
+
+cd -
