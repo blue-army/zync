3b3078860d195c4993f35fcd0927268208b16f68
rbauer <rbauer@slb.com>
2017-07-14 17:06:05 -0700

    planned graph endpoints to implement

---
+++ jibe/src/routes/api/groups/{id}/channels/{id}/threads.js
@@ -0,0 +1,5 @@
+// https://developer.microsoft.com/en-us/graph/docs/api-reference/beta/api/channel_post_chatthreads
+// POST /groups/{id}/channels/{id}/threads
+// Create a chat thread
+// Scopes: Group.ReadWrite.All
+
---
+++ jibe/src/routes/api/groups/{id}/photo.ts
@@ -0,0 +1,4 @@
+// https://developer.microsoft.com/en-us/graph/docs/api-reference/v1.0/api/profilephoto_get
+// Requires Group.Read.All
+
+
