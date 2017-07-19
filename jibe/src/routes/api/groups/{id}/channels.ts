38e57326bc87b55b263cd2efd90414749b6886b7
rbauer <rbauer@slb.com>
2017-07-19 12:14:30 -0700

    fixed list_channels function

--- jibe/src/routes/api/groups/{id}/channels.ts
+++ jibe/src/routes/api/groups/{id}/channels.ts
@@ -30,7 +30,7 @@ function list_channels(req: express.Request, res: express.Response) {
         .then((graph_res: any) => {
             // Filter out extraneous info
             console.log("Success!!!");
-            let val = graph_res.body.value;
+            let val = graph_res.value;
             for (let i in val) {
                 val[i] = models.GraphChannelInfo.fromObj(val[i]);
             }
@@ -41,7 +41,7 @@ function list_channels(req: express.Request, res: express.Response) {
         });
 };
 
-// Function to retrieve channel list via the Microsoft Graph API
+// Function to create a channel via the Microsoft Graph API
 function create_channel(req: express.Request, res: express.Response) {
     let token = _.get<string>(req, 'headers.token', undefined);
 
