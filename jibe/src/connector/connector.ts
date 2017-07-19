3614fbefe4fe141d54ca48f09a505ae699fd0982
Lucas Natraj <lnatraj@slb.com>
2017-07-19 11:11:45 -0700

    cleanup and fix cementing routing regex

--- jibe/src/connector/connector.ts
+++ jibe/src/connector/connector.ts
@@ -1,7 +1,6 @@
 // const utils = require('../utils/utils.js');
 // const faker = require('faker');
 import * as express from 'express';
-import * as rp from 'request-promise';
 import * as models from "../models/models";
 import * as jibe from '../service/jibe';
 
@@ -38,7 +37,7 @@ var events = [
         "name": "Cementing",
         "rule": {
             "path": "activity.activity_entity_type",
-            "expr": "^cementing$",
+            "expr": "^define cement job$",
         }
     },
     {
@@ -135,33 +134,32 @@ async function register(req: express.Request, res: express.Response) {
                 // look up event info in the events array
                 let event = events.find((element) => {
                     return element.name === n;
-                })
+                });
+
                 if (!event) {
                     console.log("Unknown event requested:", n);
                     continue;
                 }
+
                 let route = models.RouteInfo.fromObj({
                     path: event.rule.path,
                     expr: event.rule.expr,
                     channel: "",
                     webhook: webhook_url,
                 });
+                
                 project.routes.push(route);
             }
             jibe.upsertProject(project).then((project_info) => {
-                console.log("Project upserted!", project_info)
+                console.log("Project upserted!", project_info);
             }).catch((err) => {
-                console.log("Upsertion error", err)
+                console.log("Upsertion error", err);
             });
         })
         .catch((err) => {
             console.log(err);
         });
     
-    // Generate HTML response
-    var baseURI: string = process.env.BASE_URI || "https://jibe.azurewebsites.net";
-    var sendUrl = baseURI + "/api/messages/connector/send?group_name=" + group_name;
-
     res.render('connector.pug', {
         title: 'Connector Registered',
         appType: appType,
@@ -169,141 +167,11 @@ async function register(req: express.Request, res: express.Response) {
         state: state,
         webhookUrl: webhook_url,
     });
-
-    if (!process.env.DEBUG) {
-        // Generate a sample connector message as a "welcome"
-        var message = generateConnectorCard("Welcome", "This is a sample connector card sent to group: <b>" + group_name + "</b> via webhook: <b>" + webhook_url + "</b> using link: <b>" + sendUrl + "</b>");
-
-        // Post to connector endpoint
-        var options = {
-            method: 'POST',
-            uri: webhook_url,
-            body: message,
-            json: true
-        };
-
-        try {
-            await rp(options);
-        }
-        catch (err) {
-            console.log(err);
-        }
-    }
-
-}
-
-// Generates rich connector card.
-function generateConnectorCard(summary: string, text: string) {
-
-    if (typeof summary !== 'string') {
-        summary = 'joe created a new task';
-    }
-
-    if (typeof text !== 'string') {
-        text = 'lorem ipsum';
-    }
-
-    var ret = {
-        '@type': 'MessageCard',
-        '@context': 'http://schema.org/extensions',
-        'themeColor': '0076D7',
-        'summary': summary,
-        'sections': [{
-            'activityTitle': summary,
-            'activitySubtitle': 'On Project Jibe',
-            'activityImage': `${process.env.BASE_URI}/static/img/image${Math.floor(Math.random() * 9) + 1}.png`,
-            'text': text,
-            'facts': [
-                {
-                    'name': 'Assigned to',
-                    'value': 'Unassigned'
-                }, {
-                    'name': 'Due date',
-                    'value': Date().toString()
-                },
-                {
-                    'name': 'Status',
-                    'value': 'Not started'
-                }
-            ],
-        }],
-        'potentialAction': [
-            {
-                '@type': 'ActionCard',
-                'name': 'Add a comment',
-                'inputs': [
-                    {
-                        '@type': 'TextInput',
-                        'id': 'comment',
-                        'isMultiline': false,
-                        'title': 'Add a comment here for this task'
-                    }
-                ],
-                'actions': [
-                    {
-                        '@type': 'HttpPOST',
-                        'name': 'Add comment',
-                        'target': 'http://...'
-                    }
-                ]
-            },
-            {
-                '@type': 'ActionCard',
-                'name': 'Set due date',
-                'inputs': [
-                    {
-                        '@type': 'DateInput',
-                        'id': 'dueDate',
-                        'title': 'Enter a due date for this task'
-                    }
-                ],
-                'actions': [
-                    {
-                        '@type': 'HttpPOST',
-                        'name': 'Save',
-                        'target': 'http://...'
-                    }
-                ]
-            },
-            {
-                '@type': 'ActionCard',
-                'name': 'Change status',
-                'inputs': [
-                    {
-                        '@type': 'MultichoiceInput',
-                        'id': 'list',
-                        'title': 'Select a status',
-                        'isMultiSelect': 'false',
-                        'style': 'expanded',
-                        'choices': [
-                            { 'display': 'In Progress', 'value': '1' },
-                            { 'display': 'Active', 'value': '2' },
-                            { 'display': 'Closed', 'value': '3' }
-                        ]
-                    }
-                ],
-                'actions': [
-                    {
-                        '@type': 'HttpPOST',
-                        'name': 'Save',
-                        'target': 'http://...'
-                    }
-                ]
-            }
-        ]
-    };
-
-    return ret;
-}
-
-function puggy(_req: express.Request, res: express.Response) {
-    res.render('index.pug', { title: 'Hey', message: 'Hello there!' });
 }
 
 function init(app: express.Application) {
     app.get('/connector/setup', setup);
     app.get('/connector/register', register);
-    app.get('/puggy', puggy);
     return this;
 }
 
