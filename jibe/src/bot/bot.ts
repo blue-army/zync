f41f0f3818c95581c83ddd7199184fa3d3dd0794
rbauer <rbauer@slb.com>
2017-07-26 13:51:34 -0700

    added channel and team ID extraction

--- jibe/src/bot/bot.ts
+++ jibe/src/bot/bot.ts
@@ -6,6 +6,8 @@ var connector = new builder.ChatConnector({
     appPassword: process.env.MICROSOFT_APP_PASSWORD || "" 
 });
 
+
+
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session: any) {
     session.send("You said: %s", session.message.text);
@@ -32,22 +34,50 @@ bot.dialog('settings', [
    }
 ]);
 
-bot.on('conversationUpdate', function (message) {
-    bot.send(new builder.Message()
-        .address(message.address)
-        .text("conversationUpdate Event: " + JSON.stringify(message)));
 
-    if (message.membersAdded && message.membersAdded.length > 0) {
-        var membersAdded = message.membersAdded
-            .map(function (m) {
-                var isSelf = m.id === message.address.bot.id;
-                return (isSelf ? message.address.bot.name : m.name) || '' + ' (Id: ' + m.id + ')';
-            })
-            .join(', ');
+bot.on('conversationUpdate', function (message) {
+
+    // Extract team and channel info if the message was sent from MS Teams
+    if (message.address.channelId === "msteams") {
+        
+        // TODO: check if this is a group chat before channel extraction
+        var channel = message.address.conversation.id;
+        var team = message.sourceEvent.team;
 
         bot.send(new builder.Message()
             .address(message.address)
-            .text('Welcome ' + membersAdded));
+            .text("Team: %s | Channel: %s", team, channel));
+    }
+
+    // Display the sourceEvent
+    if (message.sourceEvent) {
+        bot.send(new builder.Message()
+            .address(message.address)
+            .text("message.sourceEvent: " + JSON.stringify(message.sourceEvent)));
+    }
+
+    if (message.membersAdded && message.membersAdded.length > 0) {
+
+        // check if the bot is in the list of new channel members
+        var botIndex = message.membersAdded.findIndex(function (element) {
+            return element.id === message.address.bot.id;
+        })
+
+        // If the bot was just added to the channel, send an introduction message
+        if (botIndex >= 0) {
+            bot.send(new builder.Message()
+                .address(message.address)
+                .text("Hello everyone! I'm Jibe."));
+        }
+        
+        // Otherwise, welcome the new channel members
+        else {
+            bot.send(new builder.Message()
+                .address(message.address)
+                .text('Welcome ' + message.membersAdded
+                    .map((m) => {return m.name;})
+                    .join(', ') + "!"));
+        }
     }
 
     if (message.membersRemoved && message.membersRemoved.length > 0) {
@@ -62,6 +92,18 @@ bot.on('conversationUpdate', function (message) {
             .address(message.address)
             .text('The following members ' + membersRemoved + ' were removed or left the conversation :('));
     }
+});
+
+bot.on('event', function (message) {
+    bot.send(new builder.Message()
+        .address(message.address)
+        .text("Event: " + JSON.stringify(message)));
+});
+
+bot.on('contactRelationUpdate', function (message) {
+    bot.send(new builder.Message()
+        .address(message.address)
+        .text("contactRelationUpdate: " + JSON.stringify(message)));
 });
 
 function sendCard(address, message) {
