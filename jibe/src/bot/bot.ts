aa0125a852c7f34a5e9d3c49dae761bd63599b28
Rebecca Bauer <rbauer@slb.com>
2017-07-25 23:40:02 -0700

    bot now reports message address

--- jibe/src/bot/bot.ts
+++ jibe/src/bot/bot.ts
@@ -9,6 +9,7 @@ var connector = new builder.ChatConnector({
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session: any) {
     session.send("You said: %s", session.message.text);
+    session.send("Your address: %s", JSON.stringify(session.message.address));
 });
 
 export {
