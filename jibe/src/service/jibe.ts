37ee51a9114993a31bdda1fd3fee14bcc3f7d1e2
rbauer <rbauer@slb.com>
2017-07-31 17:32:23 -0700

    switch to calling jibe functions from projects.ts

--- jibe/src/service/jibe.ts
+++ jibe/src/service/jibe.ts
@@ -3,15 +3,14 @@ import * as cosmos from 'documentdb';
 var docdb = require('documentdb');
 var UriFactory = docdb.UriFactory;
 
+var db_key = process.env.db_key;
+var client = new cosmos.DocumentClient('https://zync.documents.azure.com:443/', { masterKey: db_key });
+
 
 async function getProjectList(): Promise<[any]> {
 
     return new Promise<[any]>((resolve, reject) => {
-        var db_key = process.env.db_key;
-
-        let client = new cosmos.DocumentClient('https://zync.documents.azure.com:443/', { masterKey: db_key });
         var collLink = UriFactory.createDocumentCollectionUri('jibe', 'projects');
-
         var projects: any = [];
         client.readDocuments(collLink).toArray(function (err: any, docs: any) {
 
@@ -35,8 +34,6 @@ async function getProjectList(): Promise<[any]> {
 async function getProject(project_id: string): Promise<any> {
 
     return new Promise<any>((resolve, reject) => {
-        var db_key = process.env.db_key;
-        let client = new cosmos.DocumentClient('https://zync.documents.azure.com:443/', { masterKey: db_key });
         var uri = UriFactory.createDocumentUri('jibe', 'projects', project_id);
         client.readDocument(uri, function (err, doc) {
             if (err) {
@@ -53,10 +50,7 @@ async function getProject(project_id: string): Promise<any> {
 async function upsertProject(project_info: models.ProjectInfo): Promise<any> {
 
     return new Promise<any>((resolve, reject) => {
-        
         // insert document
-        var db_key = process.env.db_key;
-        let client = new cosmos.DocumentClient('https://zync.documents.azure.com:443/', { masterKey: db_key });
         var doc_uri = UriFactory.createDocumentCollectionUri('jibe', 'projects');
         client.upsertDocument(doc_uri, project_info, { disableAutomaticIdGeneration: true }, function (err:any, obj:any, _headers:any) {
             
