265185db7f4fb9f9c8f053f6b112fe26287c8f07
rbauer <rbauer@slb.com>
2017-08-04 19:49:37 -0700

    updated card creation dialog function

--- jibe/src/bot/dialogs/o365card.ts
+++ jibe/src/bot/dialogs/o365card.ts
@@ -5,12 +5,20 @@ import * as actionableCard from '../actionableCards/selectEvents'
 
 
 // Dialog for sending an o365card
-function o365CardDialog(session: builder.Session) { 
-  let card = actionableCard.createCard(session);
+async function o365CardDialog(session: builder.Session) { 
+  let card = await actionableCard.createCard(session);
+  
+  // check card was created successfully (will not be created if project retrieval from db fails)
+  if (!card) {
+    session.send("Sorry, there was a problem creating the project selection card. Please try again later.");
+    return;
+  }
+
+  // Send card
   var msg = new teams.TeamsMessage(session)
                 .summary("A sample O365 actionable card")
                 .attachments([card]);
-
+  console.log(JSON.stringify(card.toAttachment()));
   session.send(msg);
   session.endDialog();
 }
@@ -25,8 +33,11 @@ var o365CardActionHandler = function (event: builder.IEvent, query: teams.IO365C
             .textFormat("xml")
             .text(`<h2>Thanks, ${userName}!</h2><br/><h3>Your input action ID:</h3><br/><pre>${query.actionId}</pre><br/><h3>Your input body:</h3><br/><pre>${JSON.stringify(body, null, 2)}</pre>`);
   jibeBot.connector.send([msg.toMessage()], (err: Error, address?: builder.IAddress[]) => {
-
+    jibeBot.bot.send(new builder.Message()
+      .address(event.address)
+      .text(query.body))
   });
+
   callback(null, null, 200);
 }
 
