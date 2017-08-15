aee5f373527b0c12611b4cc0917d2ef546dbfa94
Lucas Natraj <lnatraj@slb.com>
2017-08-15 12:32:19 -0700

    return image

--- jibe/src/slash/slash.ts
+++ jibe/src/slash/slash.ts
@@ -2,7 +2,13 @@ import * as express from 'express';
 
 function avatar(_req: express.Request, res: express.Response) {
     res.json({
-        "text": "iroh"
+        "response_type": "ephemeral",
+        "text": "iroh",
+        "attachments": [{
+            "fallback": "Iroh",
+            "image_url": "http://files.softicons.com/download/culture-icons/avatar-minis-icons-by-joumana-medlej/png/48x48/Earthbender%20Bumi.png",
+            "thumb_url": "http://files.softicons.com/download/culture-icons/avatar-minis-icons-by-joumana-medlej/png/48x48/General%20Iroh.png"
+        }]
     })
 }
 
