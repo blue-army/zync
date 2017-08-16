837351d8f4b3bf142f241b3a4794b602ceabba2d
Lucas Natraj <lnatraj@slb.com>
2017-08-16 08:59:27 -0700

    change return code

--- jibe/src/slash/slash.ts
+++ jibe/src/slash/slash.ts
@@ -4,7 +4,7 @@ function avatar(req: express.Request, res: express.Response) {
 
     // verify request
     if (req.body['token'] != 'LFAeYBPsS5rlo2WbYxCnxPyO') {
-        return res.send(404);
+        return res.status(401).send("Unauthorized");
     }
 
     res.json({
